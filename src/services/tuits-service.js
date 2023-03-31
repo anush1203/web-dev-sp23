dd4c86f8c92e76ad4c77fd0b61729b9d13da8576
anushmadcourse <anushmadcourse@gmail.com>
2023-03-31 17:34:06 -0400

    a8 second commit

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-//const TUITS_API = "http://localhost:4000/api/tuits";
-//const TUITS_API = "https://web-dev-server-sp23.onrender.com/api/tuits";
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
